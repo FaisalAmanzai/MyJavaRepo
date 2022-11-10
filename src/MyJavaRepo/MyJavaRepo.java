51dbfc454ff4b32e0c88dc91073fcb612e6e9798
faisal.amanzai@tek.us <faisal.amanzai@tekschool.us>
2022-11-09 16:57:30 -0800

    Practice Repo

---
+++ src/MyJavaRepo/MyJavaRepo.java
@@ -0,0 +1,14 @@
+package MyJavaRepo;
+
+public class MyJavaRepo {
+
+	public static void main(String[] args) {
+		
+		
+		int num = 12;
+		double nums = 15.5;
+		System.out.println(num+nums);
+		
+	}
+
+}
